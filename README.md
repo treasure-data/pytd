48d7e6f615882986a737305e2396874ab15c92ec
Takuya Kitazawa <k.takuti@gmail.com>
2018-12-07 14:49:39 +0900

    Add prestodb.Connection wrapper for TD

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ If you don't want to introduce Kerberos system dependency, try:
 ```sh
 pip install . --process-dependency-links
 ```
+
+## Usage
+
+Set `TD_API_KEY` as an environment variable beforehand.
+
+```py
+import pytd
+
+conn = pytd.connect(database='sample_datasets')
+# or, hard-code your API key:
+# >>> pytd.connect(apikey='1/XXX', database='sample_datasets')
+
+cur = conn.cursor()
+cur.execute('select symbol, count(1) as cnt from nasdaq group by 1 order by 2 desc')
+
+rows = cur.fetchall()
+column_names = [desc[0] for desc in cur.description]
+```
--- pytd/__init__.py
+++ pytd/__init__.py
@@ -1,1 +1,30 @@
+import prestodb
+import os
+
+
 __version__ = '0.0.1'
+
+
+def connect(*args, **kwargs):
+    return Connection(*args, **kwargs)
+
+
+class Connection(object):
+
+    def __init__(self, apikey=None, database='sample_datasets'):
+        if apikey is None:
+            if 'TD_API_KEY' not in os.environ:
+                raise ValueError("either argument 'apikey' or environment variable 'TD_API_KEY' should be set")
+            apikey = os.environ['TD_API_KEY']
+
+        self.td_presto = prestodb.dbapi.connect(
+            host='api-presto.treasuredata.com',
+            port=443,
+            http_scheme='https',
+            user=apikey,
+            catalog='td-presto',
+            schema=database
+        )
+
+    def cursor(self):
+        return self.td_presto.cursor()
