9abb95f3d3801653e49e2c13691bac6bd00119b4
Takuya Kitazawa <k.takuti@gmail.com>
2018-12-07 14:57:21 +0900

    Move connection code to individual file

--- pytd/__init__.py
+++ pytd/__init__.py
@@ -1,30 +1,8 @@
-import prestodb
-import os
+from .td import connect
 
 
 __version__ = '0.0.1'
 
-
-def connect(*args, **kwargs):
-    return Connection(*args, **kwargs)
-
-
-class Connection(object):
-
-    def __init__(self, apikey=None, database='sample_datasets'):
-        if apikey is None:
-            if 'TD_API_KEY' not in os.environ:
-                raise ValueError("either argument 'apikey' or environment variable 'TD_API_KEY' should be set")
-            apikey = os.environ['TD_API_KEY']
-
-        self.td_presto = prestodb.dbapi.connect(
-            host='api-presto.treasuredata.com',
-            port=443,
-            http_scheme='https',
-            user=apikey,
-            catalog='td-presto',
-            schema=database
-        )
-
-    def cursor(self):
-        return self.td_presto.cursor()
+__all__ = [
+    'connect',
+]
---
+++ pytd/td.py
@@ -0,0 +1,27 @@
+import os
+import prestodb
+
+
+def connect(*args, **kwargs):
+    return Connection(*args, **kwargs)
+
+
+class Connection(object):
+
+    def __init__(self, apikey=None, database='sample_datasets'):
+        if apikey is None:
+            if 'TD_API_KEY' not in os.environ:
+                raise ValueError("either argument 'apikey' or environment variable 'TD_API_KEY' should be set")
+            apikey = os.environ['TD_API_KEY']
+
+        self.td_presto = prestodb.dbapi.connect(
+            host='api-presto.treasuredata.com',
+            port=443,
+            http_scheme='https',
+            user=apikey,
+            catalog='td-presto',
+            schema=database
+        )
+
+    def cursor(self):
+        return self.td_presto.cursor()
