b7455bdcb2991825bd8f84d5b011e5200eb56259
Takuya Kitazawa <k.takuti@gmail.com>
2019-04-01 16:26:47 -0700

    Single query engine per client

--- pytd/client.py
+++ pytd/client.py
@@ -1,8 +1,6 @@
 import os
-import six
 import logging
 
-from pytd.version import __version__
 from pytd.writer import SparkWriter
 from pytd.query_engine import PrestoQueryEngine, HiveQueryEngine
 
@@ -11,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class Client(object):
 
-    def __init__(self, apikey=None, endpoint=None, database='sample_datasets', default_engine='presto', header=True):
+    def __init__(self, apikey=None, endpoint=None, database='sample_datasets', engine='presto', header=True):
         if apikey is None:
             if 'TD_API_KEY' not in os.environ:
                 raise ValueError("either argument 'apikey' or environment variable 'TD_API_KEY' should be set")
@@ -22,31 +20,23 @@ class Client(object):
                 raise ValueError("either argument 'endpoint' or environment variable 'TD_API_SERVER' should be set")
             endpoint = os.environ['TD_API_SERVER']
 
-        self._connect_query_engines(apikey, endpoint, database)
+        self._connect_query_engine(apikey, endpoint, database, engine, header)
 
         self.apikey = apikey
         self.endpoint = endpoint
         self.database = database
-        self.default_engine = default_engine
         self.header = header
 
         self.writer = None
 
     def close(self):
-        self.presto.close()
-        self.hive.close()
+        self.engine.close()
         if self.writer is not None:
             self.writer.close()
 
-    def query(self, sql, engine=None):
-        if engine is None:
-            engine = self.default_engine
-        cur = self.get_cursor(engine)
-        sql = self._create_header("Client#query(engine={0})".format(engine)) + sql
-        cur.execute(sql)
-        rows = cur.fetchall()
-        columns = [desc[0] for desc in cur.description]
-        return {'data': rows, 'columns': columns}
+    def query(self, sql):
+        header = self.engine.create_header('Client#query')
+        return self.engine.execute(header + sql)
 
     def load_table_from_dataframe(self, df, table, if_exists='error'):
         if self.writer is None:
@@ -58,38 +48,16 @@ class Client(object):
 
         self.writer.write_dataframe(df, destination, if_exists)
 
-    def get_cursor(self, engine=None):
-        if engine is None:
-            engine = self.default_engine
-
-        if engine == 'presto':
-            return self.presto.cursor()
-        elif engine == 'hive':
-            return self.hive.cursor()
-        else:
-            raise ValueError('`engine` should be "presto" or "hive"')
-
     def __enter__(self):
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
         self.close()
 
-    def _connect_query_engines(self, apikey, endpoint, database):
-        self.presto = PrestoQueryEngine(apikey, endpoint.replace('api', 'api-presto'), database)
-        self.hive = HiveQueryEngine(apikey, endpoint, database)
-
-    def _create_header(self, extra_rows=None):
-        if self.header is False:
-            header = ''
-        elif isinstance(self.header, six.string_types):
-            header = "-- {0}\n".format(self.header)
+    def _connect_query_engine(self, apikey, endpoint, database, engine, header):
+        if engine == 'presto':
+            self.engine = PrestoQueryEngine(apikey, endpoint, database, header)
+        elif engine == 'hive':
+            self.engine = HiveQueryEngine(apikey, endpoint, database, header)
         else:
-            header = "-- client: pytd/{0}\n".format(__version__)
-
-        if isinstance(extra_rows, six.string_types):
-            header += "-- {0}\n".format(extra_rows)
-        elif isinstance(extra_rows, (list, tuple)):
-            header += ''.join(["-- {0}\n".format(row) for row in extra_rows])
-
-        return header
+            raise ValueError('`engine` should be "presto" or "hive"')
--- pytd/query_engine.py
+++ pytd/query_engine.py
@@ -1,14 +1,41 @@
 import abc
 import re
+import six
 import prestodb
 import tdclient
 
 from pytd.version import __version__
 
-from six import with_metaclass
 
+class QueryEngine(six.with_metaclass(abc.ABCMeta)):
 
-class QueryEngine(with_metaclass(abc.ABCMeta)):
+    def __init__(self, apikey, endpoint, database, header):
+        self.apikey = apikey
+        self.endpoint = endpoint
+        self.database = database
+        self.header = header
+
+    def execute(self, sql):
+        cur = self.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        columns = [desc[0] for desc in cur.description]
+        return {'data': rows, 'columns': columns}
+
+    def create_header(self, extra_lines=None):
+        if self.header is False:
+            header = ''
+        elif isinstance(self.header, six.string_types):
+            header = "-- {0}\n".format(self.header)
+        else:
+            header = "-- client: pytd/{0}\n".format(__version__)
+
+        if isinstance(extra_lines, six.string_types):
+            header += "-- {0}\n".format(extra_lines)
+        elif isinstance(extra_lines, (list, tuple)):
+            header += ''.join(["-- {0}\n".format(row) for row in extra_lines])
+
+        return header
 
     @abc.abstractmethod
     def cursor(self):
@@ -19,14 +46,15 @@ class QueryEngine(with_metaclass(abc.ABCMeta)):
         pass
 
     @abc.abstractmethod
-    def _connect(self, apikey, endpoint, database):
+    def _connect(self):
         pass
 
 
 class PrestoQueryEngine(QueryEngine):
 
-    def __init__(self, apikey, endpoint, database):
-        self.engine = self._connect(apikey, endpoint, database)
+    def __init__(self, apikey, endpoint, database, header):
+        super(PrestoQueryEngine, self).__init__(apikey, endpoint, database, header)
+        self.engine = self._connect()
 
     def cursor(self):
         return self.engine.cursor()
@@ -34,24 +62,25 @@ class PrestoQueryEngine(QueryEngine):
     def close(self):
         self.engine.close()
 
-    def _connect(self, apikey, endpoint, database):
+    def _connect(self):
         http = re.compile(r'https?://')
         user_agent = 'pytd/%s (Presto; prestodb/%s)' % (__version__, prestodb.__version__)
         return prestodb.dbapi.connect(
-            host=http.sub('', endpoint).strip('/'),
+            host=http.sub('', self.endpoint).strip('/'),
             port=443,
             http_scheme='https',
-            user=apikey,
+            user=self.apikey,
             catalog='td-presto',
-            schema=database,
+            schema=self.database,
             http_headers={'user-agent': user_agent}
         )
 
 
 class HiveQueryEngine(QueryEngine):
 
-    def __init__(self, apikey, endpoint, database):
-        self.engine = self._connect(apikey, endpoint, database)
+    def __init__(self, apikey, endpoint, database, header):
+        super(HiveQueryEngine, self).__init__(apikey, endpoint, database, header)
+        self.engine = self._connect()
 
     def cursor(self):
         return self.engine.cursor()
@@ -59,12 +88,12 @@ class HiveQueryEngine(QueryEngine):
     def close(self):
         self.engine.close()
 
-    def _connect(self, apikey, endpoint, database):
+    def _connect(self):
         user_agent = 'pytd/%s (Hive; tdclient/%s)' % (__version__, tdclient.__version__)
         return tdclient.connect(
-            apikey=apikey,
-            endpoint=endpoint,
-            db=database,
+            apikey=self.apikey,
+            endpoint=self.endpoint,
+            db=self.database,
             user_agent=user_agent,
             type='hive'
         )
