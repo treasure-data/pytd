7a873baa6380871b5487667c85507ac5c591a884
Takuya Kitazawa <k.takuti@gmail.com>
2019-04-02 10:37:42 -0700

    Update dbapi.Connection per Client interface chance

--- pytd/dbapi/connection.py
+++ pytd/dbapi/connection.py
@@ -1,11 +1,37 @@
+import os
+
 from ..client import Client
 from .error import NotSupportedError
 
 
 class Connection(object):
 
-    def __init__(self, apikey=None, database='sample_datasets', engine='presto'):
-        self.client = Client(apikey, database, default_engine=engine)
+    def __init__(self, apikey=None, endpoint=None, **kwargs):
+        if apikey is None:
+            if 'TD_API_KEY' not in os.environ:
+                raise ValueError("either argument 'apikey' or environment variable 'TD_API_KEY' should be set")
+            apikey = os.environ['TD_API_KEY']
+        kwargs['apikey'] = apikey
+
+        if endpoint is None:
+            if 'TD_API_SERVER' not in os.environ:
+                raise ValueError("either argument 'endpoint' or environment variable 'TD_API_SERVER' should be set")
+            endpoint = os.environ['TD_API_SERVER']
+        kwargs['endpoint'] = endpoint
+
+        self.client = Client(**kwargs)
+
+    @property
+    def apikey(self):
+        return self.client.apikey
+
+    @property
+    def endpoint(self):
+        return self.client.endpoint
+
+    @property
+    def database(self):
+        return self.client.database
 
     def close(self):
         self.client.close()
