79247755148ea7b67b3619ea5c1176c290f06f77
Takuya Kitazawa <k.takuti@gmail.com>
2019-04-08 13:04:04 -0700

    Add comments for dbapi.Connection

--- pytd/dbapi/connection.py
+++ pytd/dbapi/connection.py
@@ -3,6 +3,27 @@ from .error import NotSupportedError
 
 
 class Connection(object):
+    """The DBAPI interface to Treasure Data.
+
+    https://www.python.org/dev/peps/pep-0249/
+
+    The interface internally bundles pytd.Client. All `kwargs` are directly
+    passed to the Client interface for initialization. Implementation is
+    technically based on the DBAPI interface to Treasure Data's Presto query
+    engine, which relies on presto-python-client:
+    https://github.com/prestodb/presto-python-client.
+
+    Parameters
+    ----------
+    apikey : string, optional
+        Treasure Data API key. If not given, a value of environment variable
+        `TD_API_KEY` is used by default.
+
+    endpoint : string, optional
+        Treasure Data API server. If not given, https://api.treasuredata.com is
+        used by default. List of available endpoints is:
+        https://support.treasuredata.com/hc/en-us/articles/360001474288-Sites-and-Endpoints
+    """
 
     def __init__(self, apikey=None, endpoint=None, **kwargs):
         kwargs['apikey'] = apikey
