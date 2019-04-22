39a02c23273dad91ef9727e3099ec76c71ea9e55
Takuya Kitazawa <k.takuti@gmail.com>
2019-04-22 11:29:17 +0900

    Set logging level in the init script

--- pytd/__init__.py
+++ pytd/__init__.py
@@ -1,5 +1,11 @@
+import logging
+
 from .version import __version__
 from .client import Client
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
 
 __all__ = [
     '__version__',
