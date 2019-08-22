314a9306e487c3ae4668a1928afee986315f1204
Takuya Kitazawa <k.takuti@gmail.com>
2019-08-22 17:22:17 +0900

    Apply formetter to doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,6 @@
 import ast
 import re
 
-
 with open("../pytd/version.py", "rb") as f:
     version_re = re.compile(r"__version__\s+=\s+(.*)")
     VERSION = str(
@@ -27,9 +26,9 @@ with open("../pytd/version.py", "rb") as f:
 
 # -- Project information -----------------------------------------------------
 
-project = 'pytd'
-copyright = '2019, Arm Treasure Data'
-author = 'Arm Treasure Data'
+project = "pytd"
+copyright = "2019, Arm Treasure Data"
+author = "Arm Treasure Data"
 
 # The full version, including alpha/beta/rc tags
 release = VERSION
@@ -41,19 +40,19 @@ release = VERSION
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -61,9 +60,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
