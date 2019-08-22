9eb5241c904e6dc942113fa4cb7b11d6b40757c5
Takuya Kitazawa <k.takuti@gmail.com>
2019-08-22 17:20:57 +0900

    Add basic Sphinx extensions

--- doc/conf.py
+++ doc/conf.py
@@ -41,6 +41,10 @@ release = VERSION
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
