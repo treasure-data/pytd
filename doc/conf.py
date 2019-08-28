e20bb32b8d09186222d5d626d7f7ce3225088326
Takuya Kitazawa <k.takuti@gmail.com>
2019-08-28 12:23:33 +0900

    Hotfix: Enable autosummary_generate for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -108,6 +108,8 @@ autodoc_default_options = {
     "exclude-members": None,
 }
 
+autosummary_generate = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
