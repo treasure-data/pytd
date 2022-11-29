349360b57ac1d0ed87eaa2bd8e58df125fb44a5f
Robin Bowes <robin.bowes@yo61.com>
2022-11-29 17:47:39 +0000

    Pass python matrix version into nox tests

--- noxfile.py
+++ noxfile.py
@@ -17,12 +17,13 @@ def lint(session):
     [
         (python, pandas)
         for python in ("3.7", "3.8", "3.9", "3.10")
-        for pandas in ("1.2.0", "1.3.0", "1.4.9", "1.5.2")
-        if (python, pandas) != ("3.7", "1.4.9")
+        for pandas in ("1.2.5", "1.3.5", "1.4.4", "1.5.2")
+        if (python, pandas) != ("3.7", "1.4.4")
         if (python, pandas) != ("3.7", "1.5.2")
+        if (python, pandas) != ("3.10", "1.2.5")
     ],
 )
-def tests(session, pandas):
+def tests(session, python, pandas):
     session.install(".[test,spark]")
     session.install(f"pandas=={pandas}")
     session.run("pytest", "-v")
