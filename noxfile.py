5600cdcc5f022ff711b5c27ffb3a85e458451306
Aki Ariga <ariga@treasure-data.com>
2025-10-29 17:21:57 -0700

    Move to pyproject.toml

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import nox
 @nox.session
 def lint(session):
     lint_tools = ["black", "isort", "flake8"]
-    targets = ["pytd", "setup.py", "noxfile.py"]
+    targets = ["pytd", "noxfile.py"]
     session.install(*lint_tools)
     session.run("flake8", *targets)
     session.run("black", "--diff", "--check", *targets)
